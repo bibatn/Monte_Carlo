c838f5d7d44f2d36316480822e0238aca3608bb0
Marat_Khabibulin <marat.habibulin@nami.ru>
2022-10-28 19:13:47 +0300

    Небольшой рефакторинг.

--- main.cpp
+++ main.cpp
@@ -27,19 +27,22 @@ int main(int argc, char **argv) {
 	// Start
 	start_time = MPI_Wtime();
 
-	while (!is_tol_enough && iter_count < MAX_ITER_COUNT) {
+	while (!is_tol_enough && iter_count < MAX_ITER_COUNT)
+    {
 		// Generate sum of F func values
 		current_sum = generate_values();
 
 		MPI_Reduce(&current_sum, &all_sum, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
 
-		if (rank == MASTER) {
+		if (rank == MASTER)
+        {
 			// Master
 			count_value += all_sum;
 			res_value = VOLUME * count_value / iter_count;
 			tol = abs(I - res_value);
 
-			if (tol < eps) {
+			if (tol < eps)
+            {
 				is_tol_enough = true;
 			}
 		}
@@ -49,48 +52,48 @@ int main(int argc, char **argv) {
 	
 	prog_time = MPI_Wtime() - start_time;
 
-	if (!is_tol_enough) {
-		printf("Terminated due to max loop limit\n");
+	if (!is_tol_enough)
+    {
+        std::cout << "Пройдено максимальное кол-во итераций." << std::endl;
 	}
-	else {
-		printf("Terminated due to good sollution\n");
+	else
+    {
+        std::cout << "Решение сошлось к заданной наперед точности." << std::endl;
 	}
 
 	// Count time
 	MPI_Reduce(&prog_time, &max_prog_time, 1, MPI_DOUBLE, MPI_MAX, MASTER, MPI_COMM_WORLD);
 
 	// Display result
-	if (rank == MASTER) {
-		printf("Proc count: %d\nEPS: %.10lf\n", NODE_COUNT, eps);
-		printf("Res: %.10lf\nTol: %.10lf\nTime: %.10lf\n\n", res_value, tol, max_prog_time);
+	if (rank == MASTER)
+    {
+        std::cout << "Proc count: " << NODE_COUNT << "\nEPS: " << eps  << std::endl;
+        std::cout << "Result: " << res_value << "\nTol: " << tol << "\nTime: " << max_prog_time << std::endl;
 	}
 
-	// Exit
 	MPI_Finalize();
-
 	return 0;
 }
 
-double f_func(double x, double y, double z) {
+double f_func(double x, double y, double z)
+{
 	double res = 0;
-
 	if (z >= sqrt(x*x + y*y)) {
 		res = sqrt(x*x + y*y);
 	}
-	
 	return res;
 }
 
-double generate_values() {
+double generate_values()
+{
 	double current_sum = 0;
 	double x, y, z;
-
-	for (uint64_t i = 0; i < MAX_SAMPLES_COUNT; i++) {
+	for (uint64_t i = 0; i < MAX_SAMPLES_COUNT; i++)
+    {
 		x = (double)rand() / RAND_MAX;
 		y = (double)rand() / RAND_MAX;
 		z = (double)rand() / RAND_MAX;
 		current_sum += f_func(x, y, z);
 	}
-
 	return current_sum / (NODE_COUNT * MAX_SAMPLES_COUNT);
 }
